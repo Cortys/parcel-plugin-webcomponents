784de743fa931fc083124b27554f0b31ffafab24
Clemens Damke <admin@cortys.de>
2019-03-05 12:47:51 +0100

    release 1.1.0: updated dependencies + added support for multiple entrypoints

--- src/plugin.js
+++ src/plugin.js
@@ -5,20 +5,34 @@ const fs = require("fs-extra");
 const replace = require("replace-in-file");
 
 module.exports = bundler => {
+	function fixBundle(bundle) {
+		const { name, childBundles } = bundle;
+
+		// Multiple entrypoints:
+		if(name == null) {
+			for(const childBundle of childBundles)
+				fixBundle(childBundle);
+
+			return;
+		}
+
+		replace.sync({
+			files: name,
+			from: /(?<!<!--webcomponents-->[^]*)<script/,
+			to: `<!--webcomponents--><script src="${path.posix.join(bundler.options.publicURL, "webcomponentsjs/webcomponents-loader.js")}"></script><script`
+		});
+	}
+
 	bundler.on("bundled", bundle => {
 		fs.copySync(
 			path.dirname(require.resolve("@webcomponents/webcomponentsjs")),
-			path.join(path.dirname(bundle.name), "webcomponentsjs"),
+			path.join(bundler.options.outDir, "webcomponentsjs"),
 			{
 				dereference: true,
 				filter: file => file.match(/(webcomponentsjs(\/bundles)?|\.js\.map|\.js)$/) !== null
 			}
 		);
 
-		replace.sync({
-			files: bundle.name,
-			from: /(?<!<!--webcomponents-->[^]*)<script/,
-			to: `<!--webcomponents--><script src="${path.posix.join(bundler.options.publicURL, "webcomponentsjs/webcomponents-loader.js")}"></script><script`
-		});
+		fixBundle(bundle);
 	});
 };
