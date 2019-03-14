9061bb7a6bef0eea88d074757d98a2cdb7cbac2f
Clemens Damke <admin@cortys.de>
2019-03-14 14:04:30 +0100

    1.2.0: fixed incomplete webcomponents polyfill bundling on Windows + now all HTML assets are fixed, not just entrypoints

--- src/plugin.js
+++ src/plugin.js
@@ -6,21 +6,17 @@ const replace = require("replace-in-file");
 
 module.exports = bundler => {
 	function fixBundle(bundle) {
-		const { name, childBundles } = bundle;
+		const { name, type, childBundles } = bundle;
 
-		// Multiple entrypoints:
-		if(name == null) {
-			for(const childBundle of childBundles)
-				fixBundle(childBundle);
+		if(type === "html")
+			replace.sync({
+				files: name,
+				from: /(?<!<!--webcomponents-->[^]*)<script/,
+				to: `<!--webcomponents--><script src="${path.posix.join(bundler.options.publicURL, "webcomponentsjs/webcomponents-loader.js")}"></script><script`
+			});
 
-			return;
-		}
-
-		replace.sync({
-			files: name,
-			from: /(?<!<!--webcomponents-->[^]*)<script/,
-			to: `<!--webcomponents--><script src="${path.posix.join(bundler.options.publicURL, "webcomponentsjs/webcomponents-loader.js")}"></script><script`
-		});
+		for(const childBundle of childBundles)
+			fixBundle(childBundle);
 	}
 
 	bundler.on("bundled", bundle => {
@@ -29,7 +25,7 @@ module.exports = bundler => {
 			path.join(bundler.options.outDir, "webcomponentsjs"),
 			{
 				dereference: true,
-				filter: file => file.match(/(webcomponentsjs(\/bundles)?|\.js\.map|\.js)$/) !== null
+				filter: file => file.match(/(webcomponentsjs([/\\]bundles)?|\.js\.map|\.js)$/) !== null
 			}
 		);
 
